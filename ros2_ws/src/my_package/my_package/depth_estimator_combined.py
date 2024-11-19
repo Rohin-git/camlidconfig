424fae618679d0c4b242543272ae5c673c5dfa2d
Rohin-git <rohinjohnmathews@gmail.com>
2024-11-19 21:38:31 +0200

    modified:   ros2_ws/log/latest_build 	modified:   ros2_ws/src/my_package/my_package/lidar_publisher_rviz.py 	deleted:    ros2_ws/src/my_package/urdf/vehicle.urdf 	deleted:    ros2_ws/src/my_package/urdf/vehicle_corrected.urdf

--- ros2_ws/src/my_package/my_package/depth_estimator_combined.py
+++ ros2_ws/src/my_package/my_package/depth_estimator_combined.py
@@ -1,3 +1,7 @@
+#Part 1 : Batch synchronisation and storage : 
+#In the below code we are only considering saving LiDAR Left, Camera Front Wide and Camera Front Left. It could be further extended -
+#-to all the sensors. But for our experiment we are only considering the above. Further the below data from rosbag recording woudnt -
+# -be used for the experiment, but existing DDAD multi sensor dataset would be utilized. 
 import rclpy
 import numpy as np
 from ctypes import *
@@ -30,10 +34,10 @@ class Depth_Subscriber(Node):
             #self.lidar_sub_left, self.lidar_sub_right, self.camera_sub_back, self.camera_sub_back_left, self.camera_sub_back_right, self.camera_sub_front_left, self.camera_sub_front_right, self.camera_sub_front_narrow, self.camera_sub_front_wide
         self.sync.registerCallback(self.callback_all_topics)
         print("After sync synchromiser")
-        #self.publisher = self.create_publisher(Bool, '/bool_set_01', 10)
+        
         self.i = 0
         self.n = 0
-        #self.bool_msg = Bool()
+        
         
         
         #self.timer = self.create_timer(60.0, self.save_point_cloud) # set a timer to determine the duration of each batch
@@ -45,17 +49,7 @@ class Depth_Subscriber(Node):
     
         print(f"Saving batch {self.i}") # to confirm entry to save batch function
         file_path = f'/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/batch_files/concatenated_point_cloud_lid_cam_{self.i}.npz'
-        # for timestamp, data in self.dictionary.items():
-        #     print("Timestamp:", timestamp)
-        #     # Iterate over the keys and values in the inner dictionary
-        #     if self.n ==1:
-        #         for item in data:
-        #             camera_name = item["camera_name"]
-        #             image = item["image"]
-        #             print(camera_name ,":", image)
-        #     else:
-        #         for key, value in data.items():
-        #             print(key, ":", value)
+        
         # Convert keys to strings if they are not already strings
         string_keys_dict = {str(key): value for key, value in self.dictionary.items()}
 
@@ -75,22 +69,7 @@ class Depth_Subscriber(Node):
 
             # Convert PointCloud2 message to NumPy array
             cloud_data_lidar_left = np.frombuffer(lidarleft_msg.data, dtype=np.float32).reshape(-1, lidarleft_msg.point_step // 4)
-            # # Extract XYZ coordinates
-            # xyz = cloud_data[:, :3]
-            # # Create Open3D point cloud
-            # pcd = o3d.geometry.PointCloud()
-            # pcd.points = o3d.utility.Vector3dVector(xyz)
-            # self.lidar_left.append(pcd)                       # Data appended to the list for Lidar left
-            # #For Lidar right
-
-            # # Convert PointCloud2 message to NumPy array
-            # cloud_data = np.frombuffer(lidar_02_msg.data, dtype=np.float32).reshape(-1, lidar_02_msg.point_step // 4)
-            # # Extract XYZ coordinates
-            # xyz = cloud_data[:, :3]
-            # # Create Open3D point cloud
-            # pcd = o3d.geometry.PointCloud()
-            # pcd.points = o3d.utility.Vector3dVector(xyz)
-            # self.lidar_right.append(pcd)                     # data appended to list for Lidar right
+            
             ################################### Camera point cloud ###################################################################
 
             # Camera Front Wide
@@ -140,50 +119,7 @@ class Depth_Subscriber(Node):
                 self.dictionary[timestamp_lidar_left] = {"lidar_name": "left", "image": cloud_data_lidar_left}
                 
 
-            ############################################ camera_backright
-            # cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(camera_backright_msg)   # Convert the image to cv2
-            # with open('/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/intrinsic/camera_back_right.yaml', 'r') as file:
-            #     camera_data = yaml.safe_load(file)            
-            #     camera_matrix = np.array(camera_data['camera_matrix']['data']).reshape(3, 3)       # Extract camera matrix and distortion coefficients from the YAML file
-            #     dist_coeffs = np.array(camera_data['distortion_coefficients']['data'])
-            # rectified_image = cv2.undistort(cv_image, camera_matrix, dist_coeffs)     
-            # self.camera_back_right.append(rectified_image) 
-
-            # ############################################ camera_front left
-            # cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(camera_frontleft_msg)   # Convert the image to cv2
-            # with open('/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/intrinsic/camera_front_left.yaml', 'r') as file:
-            #     camera_data = yaml.safe_load(file)            
-            #     camera_matrix = np.array(camera_data['camera_matrix']['data']).reshape(3, 3)       # Extract camera matrix and distortion coefficients from the YAML file
-            #     dist_coeffs = np.array(camera_data['distortion_coefficients']['data'])
-            # rectified_image = cv2.undistort(cv_image, camera_matrix, dist_coeffs)     
-            # self.camera_front_left.append(rectified_image) 
-
-            # ############################################ camera_front right
-            # cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(camera_frontright_msg)   # Convert the image to cv2
-            # with open('/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/intrinsic/camera_front_right.yaml', 'r') as file:
-            #     camera_data = yaml.safe_load(file)            
-            #     camera_matrix = np.array(camera_data['camera_matrix']['data']).reshape(3, 3)       # Extract camera matrix and distortion coefficients from the YAML file
-            #     dist_coeffs = np.array(camera_data['distortion_coefficients']['data'])
-            # rectified_image = cv2.undistort(cv_image, camera_matrix, dist_coeffs)     
-            # self.camera_front_right.append(rectified_image) 
-
-            # ############################################ camera_front narrow
-            # cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(camera_frontnarrow_msg)   # Convert the image to cv2
-            # with open('/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/intrinsic/camera_front_narrow.yaml', 'r') as file:
-            #     camera_data = yaml.safe_load(file)            
-            #     camera_matrix = np.array(camera_data['camera_matrix']['data']).reshape(3, 3)       # Extract camera matrix and distortion coefficients from the YAML file
-            #     dist_coeffs = np.array(camera_data['distortion_coefficients']['data'])
-            # rectified_image = cv2.undistort(cv_image, camera_matrix, dist_coeffs)     
-            # self.camera_front_narrow.append(rectified_image) 
-
-            # ############################################ camera_front wide
-            # cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(camera_frontwide_msg)   # Convert the image to cv2
-            # with open('/home/rohin/camlid/camlidconfig/ros2_ws/src/my_package/intrinsic/camera_front_wide.yaml', 'r') as file:
-            #     camera_data = yaml.safe_load(file)            
-            #     camera_matrix = np.array(camera_data['camera_matrix']['data']).reshape(3, 3)       # Extract camera matrix and distortion coefficients from the YAML file
-            #     dist_coeffs = np.array(camera_data['distortion_coefficients']['data'])
-            # rectified_image = cv2.undistort(cv_image, camera_matrix, dist_coeffs)     
-            # self.camera_front_wide.append(rectified_image)  
+            
         self.counter += 1 
         if self.i == 0:
 
